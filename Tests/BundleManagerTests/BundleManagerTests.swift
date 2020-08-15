07278ebfcbea135a25b5821ba503cef37a67987b
Christian Seiler <christian@christianseiler.ch>
2020-08-15 19:00:32 +0200

    Add Travis CI Config FIle

--- Tests/BundleManagerTests/BundleManagerTests.swift
+++ Tests/BundleManagerTests/BundleManagerTests.swift
@@ -2,11 +2,10 @@ import XCTest
 @testable import BundleManager
 
 final class BundleManagerTests: XCTestCase {
+
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(BundleManager().text, "Hello, World!")
+        XCTAssertEqual(BundleManager.default.bundleName, nil)
     }
 
     static var allTests = [
